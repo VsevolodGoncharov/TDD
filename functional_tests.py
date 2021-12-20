be4aae3b0a8b79ed918c43d795efcb3762b6963d
vsevolod_goncharov <gyro7931@gmail.com>
2021-12-21 02:12:29 +0300

    HTML главной страницы теперь генерируется из шаблона

--- functional_tests.py
+++ functional_tests.py
@@ -40,7 +40,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
         self.assertTrue(
-            any(row.text == '1: Прополоть репу' for row in rows)
+            any(row.text == '1: Прополоть репу' for row in rows),
+            'Новый элемент списка не появился в таблице'
         )
 
         # Текстовое поле по-прежнему приглашает её добавить ещё один элемент.
