705772d2de405f22c7974291d2a6276854e1f52d
vsevolod_goncharov <gyro7931@gmail.com>
2021-12-21 01:40:49 +0300

    ФТ теперь проверяет возможность ввести элемент списка

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
-from selenium import webdriver
+import time
 import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,17 +19,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # Она видит, что заголовок и шапка страницы говорят о списках неотложных дел
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ей сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Она набирает в текстовом поле "Прополоть репу"
+        inputbox.send_keys('Прополоть репу')
 
         # Когда она нажимает enter, страница обновляется,
         # и теперь страница содержит "1: Прополоть репу" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Прополоть репу' for row in rows)
+        )
 
         # Текстовое поле по-прежнему приглашает её добавить ещё один элемент.
         # Она вводит "Помыть кобылу"
+        self.fail('Закончить тест!')
 
         # Страница снова обновляется, и теперь показывает оба элемента
 
